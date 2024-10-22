1f8525837be41ac609a09b5bbb8a23391540d69e
Erica Hughberg <erica.sundberg.90@gmail.com>
2024-10-22 15:59:35 -0400

    trying things
    Signed-off-by: Erica Hughberg <erica.sundberg.90@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,12 +8,15 @@ USER_UID=${LOCAL_UID:-1000}
 USER_NAME=${LOCAL_USER_NAME:-buildbazel}
 USER_HOME=${LOCAL_USER_HOME:-"/home/${USER_NAME}"}
 
-echo "Starting (${*}) with user: $USER_UID $USER_NAME $USER_HOME"
+echo "Starting this stuff (${*}) with user: $USER_UID $USER_NAME $USER_HOME"
+echo "User Home = $USER_HOME"
 
 usermod -l "$USER_NAME" -u "$USER_UID" -md "$USER_HOME"  "$DEFAULT_USER_NAME" || :
 
-chown "$USER_NAME" "$USER_HOME" "${USER_HOME}/.cache/bazel" "${USER_HOME}/.cache/bazelisk"
+chown "$USER_NAME" "$USER_HOME"
 
 export HOME="${USER_HOME}"
+
+echo "Home = $HOME"
 
 exec /usr/sbin/gosu "${USER_NAME}" "$@"
